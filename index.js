a3efca686c0cca0c985f7a272938ad5d497e8c63
Miguel Victor <miguel.victor@edu.unifor.br>
2021-04-22 21:48:37 -0300

    realizei alterações de mensagens

--- index.js
+++ index.js
@@ -4,22 +4,17 @@ const app = express(); //iniciando o express
 //criando a rota inicial
 app.get("/", function(req,res){
     res.send(
-        "<h1>Bem vindo ao nosso site!</h1>" +
+        "<h1>Bem vindo ao meu site!</h1>" +
          "<hr>" +
-         "<h2>- 1810510 - Miguel Victor</h2>" +
-         "<br>" +
-         "<h2> - 1910462 - Caio Barroso</h2>" +
-         "<br>" +
-         "<h2> - 1710583 - Gabriel Jehnen</h2>" +
-         "<hr>");
+         "<h2>- 1810510 - Miguel Victor</h2>");
 })
 
 //rota pra cadastrr produto
 app.get("/produtos", function(req,res){
-    res.send("<h1>Lista de produtos</h1>");
+    res.send("<h1>Lista de produtos teste 2</h1>");
 })
 
-app.listen(3001,function(erro){  // cria a aplicação na porta 3001
+app.listen(3009,function(erro){  // cria a aplicação na porta 3001
     if (erro){
         console.log("Erro ao Iniciar.");
     }else{
